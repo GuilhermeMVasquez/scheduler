e5e3fbcf0c4c169a1eb29b148375fb080c4ac52a
Pedro Mezacasa Muller <pedro.mezzacasa@gmail.com>
2024-09-15 23:49:00 -0300

    API básica do scheduler

--- Scheduler.h
+++ Scheduler.h
@@ -2,13 +2,21 @@
 #define SCHEDULER_H
 
 #include "ReadyQueue.h"
+#include "BlockedQueue.h"
 #include "Process.h"
 
 typedef struct Scheduler {
     ReadyQueue *ready;    // Queue for ready processes
-    
+    BlockedQueue *blockedQueue; // 
+
     Process *exited;          // Pointer to the last exited process
     Process *running;         // Pointer to the currently running process
 } Scheduler;
+
+Scheduler *initScheduler();
+
+void addProcesses(Scheduler *scheduler, Process **arrayOfProcesses, int arrayCount);
+
+void timerInterrupt();
 
 #endif // SCHEDULER_H
